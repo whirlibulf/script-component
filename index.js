ef6329ef7f8e4bda2687e0f76dc37ef0ceed96ab
whirlibulf <whirlibulf.engine@gmail.com>
2013-07-08 16:00:30 +0100

    Update code

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function ComponentFactory(options) {
-  var scripts = options.scripts || [];
-
   //instance.scripts should be an array of objects that have an update method
   return {
-    "scripts": scripts
+    "scripts": options.scripts || []
   };
 }
 
