60f35175b46460a2d8efb84a3166a39040351d39
whirlibulf <whirlibulf.engine@gmail.com>
2013-07-08 22:36:19 +0100

    Update for new engine version

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function ComponentFactory(options) {
-  //instance.scripts should be an array of objects that have an update method
-  return {
-    "scripts": options.scripts || []
-  };
+    //instance.scripts should be an array of objects that have an update method
+    if (options.scripts) {
+        this.scripts = options.scripts;
+    }
+
+    if (this.scripts === undefined) {
+        this.scripts = [];
+    }
 }
 
 module.exports = ComponentFactory;
